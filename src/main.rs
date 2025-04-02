03cb4d737b61cc58e6cb5bc1ba5b089082607241
Alexander Evgin <aleasims@gmail.com>
2025-04-03 03:10:53 +0400

    Some tarpaulin configs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use clap::Parser;
 
+#[cfg(not(tarpaulin_include))]
 mod cli;
 
 #[tokio::main]
+#[cfg(not(tarpaulin_include))]
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt()
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
