b93596e624e738fabb3db61f63db9ad5f6248063
Alexander Evgin <aleasims@gmail.com>
2025-04-03 19:59:19 +0400

    Fix integration tests

--- tests/client.rs
+++ tests/client.rs
@@ -1,12 +1,23 @@
 //! Client integration tests.
 
-use std::path::PathBuf;
 use std::process::{Command, Stdio};
 use tempdir::TempDir;
 use tokio::fs;
 
 use carol::pool::{Pool, PoolManager};
 use carol::{Client, File};
+
+/// Just an example of files usage.
+async fn cat_files<'a, I>(files: I) -> anyhow::Result<()>
+where
+    I: IntoIterator<Item = &'a File>,
+{
+    Command::new("cat")
+        .args(files.into_iter().map(|file| file.cache_path().as_os_str()))
+        .stdout(Stdio::null())
+        .status()?;
+    Ok(())
+}
 
 #[tokio::test]
 async fn test_with_one_file() -> anyhow::Result<()> {
@@ -23,28 +34,7 @@ async fn test_with_one_file() -> anyhow::Result<()> {
     // This variable will keep a reference to source URL, preventing it from removal.
     let file = client.get(source_url).await?;
 
-    // We will use this function to own the file and drop it at the end.
-    // After calling this function, file will be released and can be removed by client.
-    // Instead of that you can call file.release() or drop(file) manually.
-    async fn use_file(file: File) -> anyhow::Result<()> {
-        let target = PathBuf::from("example.html");
-
-        file.symlink(&target).await?;
-
-        Command::new("cat")
-            .arg(target.as_os_str())
-            .stdout(Stdio::null())
-            .status()?;
-
-        fs::remove_file(&target).await?;
-        Ok(())
-    }
-
-    use_file(file).await?;
-
-    // Now because file using this URL was dropped, URL can be removed from cache.
-    // If the file wasn't drop, this function will be stuck waiting for file to be release.
-    client.remove(source_url).await?;
+    cat_files([&file]).await?;
 
     Ok(())
 }
@@ -80,28 +70,7 @@ async fn test_with_many_files() -> anyhow::Result<()> {
         .into_iter()
         .collect::<Result<Vec<_>, _>>()?;
 
-    // Use files as in previous test
-    async fn use_files(files: &[File]) -> anyhow::Result<()> {
-        for (id, file) in files.iter().enumerate() {
-            let target = PathBuf::from(format!("example.{}.html", id));
-
-            file.symlink(&target).await?;
-
-            Command::new("cat")
-                .arg(target.as_os_str())
-                .stdout(Stdio::null())
-                .status()?;
-
-            fs::remove_file(&target).await?;
-        }
-        Ok(())
-    }
-
-    use_files(&files).await?;
-
-    // // Now because file using this URL was dropped, URL can be removed from cache.
-    // // If the file wasn't drop, this function will be stuck waiting for file to be release.
-    // client.remove(source_url).await?;
+    cat_files(&files).await?;
 
     Ok(())
 }
